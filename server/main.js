12a427f349b0233e28a262a511aa679a318f8f08
xmoli <fromlili@outlook.com>
2020-07-28 12:35:46 +0800

    server config

--- server/main.js
+++ server/main.js
@@ -2,11 +2,19 @@ const Koa = require('koa')
 const server = require('koa-static')
 const proxy = require('koa-proxy')
 const config = require('../server.config')
+const path = require('path')
 
 const app = new Koa();
 
-app.use(server(__dirname+'../dist'))
+app.use(
+    server(
+        path.join(__dirname, '../dist')
+        )
+    )
+
+//反向代理后端api
 app.use(proxy(config.server.koaProxy))
+
 app.listen(config.server.port)
 
 console.log("port at,", config.server.port)
