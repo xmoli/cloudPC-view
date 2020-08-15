294a7e80f35e1f64a696d148927da81684e1fb67
xmoli <fromlili@outlook.com>
2020-08-15 17:41:42 +0800

    重新定义log的模板

--- src/components/LogFilter.vue
+++ src/components/LogFilter.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="log-filter">
         <select class="level" v-model="level" @change="$emit('change',level)">
-            <option value="error">ERROR</option>
-            <option value="debug">DEBUG</option>
-            <option value="info">INFO</option>
+            <option v-for=" item in levelItem" :key="item.name" :value="item.value">
+                {{item.name}}
+            </option>
         </select>
-        <select class="type" v-model="type" @change="$emit('change', type)">
-            <option value="task">TASK</option>
-            <option value="file">FILE</option>
+        <select class="type" v-model="type" @type="$emit('change', type)">
+            <option v-for="item in typeItem" :key="item.name" :value="item.value">
+                {{item.name}}
+            </option>
         </select>
         <div class="day">
             今日
@@ -19,8 +20,36 @@ export default {
     data() {
         return {
             level: null,
-            type: null
+            type: null,
+            levelItem: [
+                {
+                    value: 'error',
+                    name: 'ERROR'
+                },
+                {
+                    value: 'debug',
+                    name: 'DEBUG'
+                },
+                {
+                    value: 'info',
+                    name: 'INFO'
+                }
+            ],
+            typeItem: [
+                {
+                    value: 'task',
+                    name: 'TASK'
+                },
+                {
+                    value: 'file',
+                    name: 'NAME'
+                }
+            ],
         }
+    },
+    created() {
+        this.level = this.levelItem[0].value
+        this.type = this.typeItem[0].value
     }
 }
 </script>
